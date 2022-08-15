e6482fa167073818b8f326b888e73aec16b5c491
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-16 00:14:19 +0900

    feat: add SpawnPoint exts

---
+++ src/main/java/click/seichi/originspawn/presenter/SpawnLocationExt.kt
@@ -0,0 +1,14 @@
+package click.seichi.originspawn.presenter
+
+import click.seichi.originspawn.*
+import org.bukkit.Bukkit
+import org.bukkit.Location
+
+fun Location.toSpawnPoint() =
+    SpawnPoint(world.name, LocX(x), LocY(y), LocZ(z), Yaw(yaw), Pitch(pitch))
+
+fun SpawnPoint.toBukkitLocation() = Bukkit.getWorld(worldName)?.let { world ->
+    Location(world, x.value, y.value, z.value, yaw.value, pitch.value)
+}
+
+fun SpawnPoint.format() = "${worldName}: ${x.value} ${y.value} ${z.value} (${yaw.value}, ${pitch.value})"
