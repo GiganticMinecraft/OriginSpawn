d9ea3beeef32ffb515bc16534a00d732b7b1bee2
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-15 13:46:36 +0900

    refactor: use kotlin

--- src/main/java/click/seichi/originspawn/OriginSpawn.kt
+++ src/main/java/click/seichi/originspawn/OriginSpawn.kt
@@ -1,17 +1,13 @@
-package click.seichi.originspawn;
+package click.seichi.originspawn
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPlugin
 
-public final class OriginSpawn extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
+class OriginSpawn : JavaPlugin() {
+    override fun onEnable() {
         // Plugin startup logic
-
     }
 
-    @Override
-    public void onDisable() {
+    override fun onDisable() {
         // Plugin shutdown logic
     }
 }
