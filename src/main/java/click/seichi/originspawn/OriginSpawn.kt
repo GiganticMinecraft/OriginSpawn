10a737de0da248518c8d5dcd8363dcdadbb6e12b
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-08-16 00:15:11 +0900

    feat: add onEnable impls

--- src/main/java/click/seichi/originspawn/OriginSpawn.kt
+++ src/main/java/click/seichi/originspawn/OriginSpawn.kt
@@ -1,13 +1,23 @@
 package click.seichi.originspawn
 
+import click.seichi.originspawn.domain.SpawnPointPersistence
+import click.seichi.originspawn.infra.SpawnPointPersistenceImpl
+import click.seichi.originspawn.presenter.commands.Command
+import click.seichi.originspawn.presenter.shared.ServerLogger
 import org.bukkit.plugin.java.JavaPlugin
 
 class OriginSpawn : JavaPlugin() {
-    override fun onEnable() {
-        // Plugin startup logic
+    companion object {
+        lateinit var SPAWN_POINT_PERSISTENCE: SpawnPointPersistence
+            private set
     }
 
-    override fun onDisable() {
-        // Plugin shutdown logic
+    override fun onEnable() {
+        SPAWN_POINT_PERSISTENCE = SpawnPointPersistenceImpl(this)
+        SPAWN_POINT_PERSISTENCE.load()
+
+        getCommand("originspawn").executor = Command
+
+        ServerLogger.info("OriginSpawn has been enabled.")
     }
 }
