a16c1f73527e0db116b3e02af54b927a43d3410c
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-09-02 17:37:53 +0900

    fmt: run fmt

--- src/main/kotlin/click/seichi/originspawn/presenter/commands/Spawn.kt
+++ src/main/kotlin/click/seichi/originspawn/presenter/commands/Spawn.kt
@@ -4,27 +4,11 @@ import click.seichi.originspawn.OriginSpawn.Companion.SPAWN_POINT_PERSISTENCE
 import click.seichi.originspawn.presenter.format
 import click.seichi.originspawn.presenter.shared.MessageSender
 import click.seichi.originspawn.presenter.shared.ServerLogger
+import click.seichi.originspawn.presenter.shared.Teleporter
 import click.seichi.originspawn.presenter.toBukkitLocation
 import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause
 
 object Spawn : CommandExecutor("", "設定されたスポーン地点にテレポートします。", setOf("originspawn.spawn")) {
-    override fun execute(sender: Player) {
-        val messageSender = MessageSender(sender)
-        if (!hasPermissions(sender)) {
-            messageSender.severe("このコマンドを実行する権限がありません。")
-            return
-        }
-
-        val spawnPoint = SPAWN_POINT_PERSISTENCE.get()
-        val location = spawnPoint.toBukkitLocation() ?: run {
-            val errorMsg = "スポーン地点の設定を読み込めませんでした。"
-            messageSender.severe(errorMsg)
-            ServerLogger.severe(errorMsg, spawnPoint.format())
-            return
-        }
-
-        messageSender.info("サーバーのスポーン地点にテレポートしました。")
-        sender.teleport(location, TeleportCause.PLUGIN)
-    }
+    override fun execute(sender: Player) = Teleporter.run(sender)
 }
